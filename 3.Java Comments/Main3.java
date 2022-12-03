ca776daf1c92e3440334b45f7ef7fd097154fa3e
udaraayeshan <ayeshan98uara@gmail.com>
2022-12-03 13:30:27 -0800

    Comments

---
+++ 3.Java Comments/Main3.java
@@ -0,0 +1,14 @@
+// Single-line Comments
+
+public class Main3 {
+    public static void main(String[] args) {
+      System.out.println("Hello World"); // This is a comment
+
+      // Java Multi-line Comments
+      
+      /* The code below will print the words Hello World
+to the screen, and it is amazing */
+System.out.println("Java Multi-line Comments");
+    }
+  }
+  
